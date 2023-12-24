c952b3f090eb928a5915acee572257751b127a8e
Jackisapi <getphyk3d@gmail.com>
2023-12-23 17:48:23 -0700

    Things lol

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gpt4all-cli
+An unofficial CLI Writen by yours truly Jackisapi. I hope to use this for a possible senior project
+But Worst case scenario i made something kinda cool. Any Feedback is apriciated. 
--- main.py
+++ main.py
@@ -16,7 +16,16 @@ def ask_ai(quere, model):
         response = model.generate(prompt=quere)
         return response
 
-
+"""
+*************************************************************
+Opens "models2.json from the GPT4All Project and prints all
+The the Data lables Name filename type Desc and url from 
+each object 
+Parameters File ("Function Can however be called without
+Data file being defined")
+Returns: All the Models officialy supported by GPT4ALL
+*************************************************************
+"""
 def all_models(datafile='models2.json'):
     with open (datafile,'r') as model_data:
         model_dict = json.load(model_data)
@@ -28,17 +37,34 @@ def all_models(datafile='models2.json'):
                     'URL:',dict_model['description'],'\n')
         model_data.close()
 
+"""
+*************************************************************
+In development (aka no workie) at the moment butin theory
+takes a String Checks to see if that string is a available 
+model for gpt4all if not found it will throw an error
+Parameters: Name, datafile(doesnt need filled)
+Returns: Currently just the name of the model if found
+*************************************************************
+"""
 def model_check(name,datafile='models2.json'):
     with open(datafile,'r') as model_data:
         model_dict = json.load(model_data)
         for dict_model in model_dict:
             if dict_model['name'].upper() in name.upper():
-                return dict_model['name']
+                return dict_model['filename']
             else:
                 continue
 
 
-
+"""
+*************************************************************
+Asks the user for input and returns that input however
+if the input is ls it calls all_models Soon to be exteneded 
+with model_check as seen above 
+Params: None
+Returns: User Input
+*************************************************************
+"""
 def change_model():
     change_made = False
     while not change_made:
@@ -46,18 +72,21 @@ def change_model():
         if 'ls' in model:
             all_models()
         else:
+            model = model_check(model)
             return model
+
 
 
 if __name__ == "__main__":
     model = 'orca-2-7b.Q4_0.gguf'
     while True:
         prompt = input("Hello What You Want ")
-        if 'ls' in prompt:
+        if prompt == 'ls':
             all_models('models2.json')
-        elif 'ch' in prompt:
+        elif prompt == 'ch':
             model = change_model()
             print(model)
         else:
             print(ask_ai(prompt, model))
-print(model_check("Mistral OpenOrca"))
+
+
